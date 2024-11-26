cdacfc2545043bffc1fb4bcd2949a3bac0900a2e
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-26 12:25:12 -0500

    Fix fake event

--- src/screens/EventListScreen/index.tsx
+++ src/screens/EventListScreen/index.tsx
@@ -1,25 +1,26 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 25 Nov 2024, 3:43:02 PM
- *  Last update: 25 Nov 2024, 7:39:53 PM
+ *  Last update: 26 Nov 2024, 12:25:04 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { useState } from "react";
 
 import { View, Text, TouchableOpacity } from "react-native";
 
+import { Timestamp } from "firebase/firestore";
+
 import { EventDetailScreen } from "../EventDetailScreen";
 
 export function EventListScreen(): JSX.Element {
-    const [detailsShown, setDetailsShown] = useState(false);
+    const [detailsShown, setDetailsShown] = useState<boolean>(false);
     const fakeEvent = {
         id: "1",
-        author: "Blob",
-        checkedOut: false,
-        coverURL: "string",
-        description: "string",
-        rating: 2,
+        creatorID: "108310",
         title: "Title",
+        description: "Description",
+        startInstant: new Timestamp(100, 0),
+        location: "a place",
     };
 
     return (
