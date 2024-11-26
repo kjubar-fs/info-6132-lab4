7f0ee1fca945834fb7e8ec1658016fa2a59a5cfb
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-26 12:53:04 -0500

    Use placeholder data from list

--- src/screens/EventListScreen/index.tsx
+++ src/screens/EventListScreen/index.tsx
@@ -1,36 +1,29 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 25 Nov 2024, 3:43:02 PM
- *  Last update: 26 Nov 2024, 12:25:04 PM
+ *  Last update: 26 Nov 2024, 12:51:28 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { useState } from "react";
 
 import { View, Text, TouchableOpacity } from "react-native";
 
-import { Timestamp } from "firebase/firestore";
+import { useEventList } from "../../data/state/hooks";
 
 import { EventDetailScreen } from "../EventDetailScreen";
 
 export function EventListScreen(): JSX.Element {
     const [detailsShown, setDetailsShown] = useState<boolean>(false);
-    const fakeEvent = {
-        id: "1",
-        creatorID: "108310",
-        title: "Title",
-        description: "Description",
-        startInstant: new Timestamp(100, 0),
-        location: "a place",
-    };
+    const events = useEventList();
 
     return (
         <View style={{flex: 1}}>
-            <Text>Event list here</Text>
+            <Text>{events[0].title}</Text>
             <TouchableOpacity onPress={() => setDetailsShown(true)}>
                 <Text>Go to detail</Text>
             </TouchableOpacity>
 
-            <EventDetailScreen visible={detailsShown} close={() => setDetailsShown(false)} event={fakeEvent} />
+            <EventDetailScreen visible={detailsShown} close={() => setDetailsShown(false)} event={events[0]} />
         </View>
     );
 }
