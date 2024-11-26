109abf8e8543a620a341ae18f9d27994d304c8b6
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-25 22:51:40 -0500

    Create email validation

---
+++ src/util/functions.ts
@@ -0,0 +1,18 @@
+/*
+ *  Author: Kaleb Jubar
+ *  Created: 25 Nov 2024, 9:36:49 PM
+ *  Last update: 25 Nov 2024, 9:42:28 PM
+ *  Copyright (c) 2024 Kaleb Jubar
+ */
+
+/**
+ * Check if the given email is valid
+ * @param email email to validate
+ * @returns true if valid, false if not
+ */
+export function validateEmail(email: string): boolean {
+    // expansive regex that covers basically every email, taken from https://stackoverflow.com/a/201378
+    const emailRegex = /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+    return emailRegex.test(email);
+}
