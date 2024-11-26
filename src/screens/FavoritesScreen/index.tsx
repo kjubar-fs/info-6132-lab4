58ad7b41b6f9ed6ddda0cda220a522eb5399609b
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-26 12:56:45 -0500

    Add temp dummy data to favorites screen

--- src/screens/FavoritesScreen/index.tsx
+++ src/screens/FavoritesScreen/index.tsx
@@ -1,13 +1,17 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 25 Nov 2024, 3:43:02 PM
- *  Last update: 25 Nov 2024, 7:13:20 PM
+ *  Last update: 26 Nov 2024, 12:56:25 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { Text } from "react-native";
 
+import { useFavorites } from "../../data/state/hooks";
+
 export function FavoritesScreen(): JSX.Element {
+    const favorites = useFavorites();
+
     return (
-        <Text>Favorites here</Text>
+        <Text>{favorites[0].title}</Text>
     );
 }
