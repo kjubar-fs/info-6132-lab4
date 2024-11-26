7dd0bc6023cb62ae76ec852a9c8731c80e8b8e39
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-26 13:40:44 -0500

    Add basic entry fields and validation for sign-up

--- src/screens/CreateAccountScreen/index.tsx
+++ src/screens/CreateAccountScreen/index.tsx
@@ -1,10 +1,12 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 26 Nov 2024, 1:00:55 PM
- *  Last update: 26 Nov 2024, 1:17:37 PM
+ *  Last update: 26 Nov 2024, 1:40:08 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
-import { Text, TouchableOpacity } from "react-native";
+import { useEffect, useState } from "react";
+
+import { Text, TouchableOpacity, View, TextInput } from "react-native";
 
 import { GenericModal } from "../../components/common/GenericModal";
 
@@ -18,9 +20,117 @@ interface Props {
 }
 
 export function CreateAccountScreen({ visible, close }: Props): JSX.Element {
+    const [email, setEmail] = useState<string | undefined>(undefined);
+    const [emailError, setEmailError] = useState<string>("");
+    const [password, setPassword] = useState<string | undefined>(undefined);
+    const [passwordError, setPasswordError] = useState<string>("");
+    const [passwordVerify, setPasswordVerify] = useState<string | undefined>(undefined);
+    const [passwordVerifyError, setPasswordVerifyError] = useState<string>("");
+
+    // hook up an effect to clear the modal when it appears
+    useEffect(() => {
+        if (visible === true) {
+            setEmail(undefined);
+            setEmailError("");
+            setPassword(undefined);
+            setPasswordError("");
+            setPasswordVerify(undefined);
+            setPasswordVerifyError("");
+        }
+    }, [visible]);
+
+    const changeEmail = (newEmail: string) => {
+        // always update email state
+        setEmail(newEmail);
+
+        // check if required error is necessary
+        if (!newEmail) {
+            setEmailError("Required");
+        } else {
+            setEmailError("");
+        }
+    };
+
+    const changePassword = (newPass: string) => {
+        // always update password state
+        setPassword(newPass);
+
+        // check if required error is necessary
+        if (!newPass) {
+            setPasswordError("Required");
+        } else {
+            setPasswordError("");
+        }
+    };
+
+    const changePasswordVerify = (newPass: string) => {
+        // always update password state
+        setPasswordVerify(newPass);
+
+        // check if required error is necessary
+        if (!newPass) {
+            setPasswordVerifyError("Required");
+        } else {
+            setPasswordVerifyError("");
+        }
+    };
+
     return (
         <GenericModal visible={visible} cardStyles={styles.container}>
-            <Text>User sign-up</Text>
+            <View style={styles.containerInput}>
+                <View style={styles.containerCaption}>
+                    <Text style={styles.caption}>Email</Text>
+                    {emailError &&
+                        <Text style={styles.error}>{emailError}</Text>}
+                </View>
+
+                <TextInput
+                    style={styles.input}
+                    value={email}
+                    onChangeText={changeEmail}
+                    keyboardType="email-address"
+                    autoComplete="email"
+                    autoCorrect={false}
+                    autoCapitalize="none"
+                />
+            </View>
+
+            <View style={styles.containerInput}>
+                <View style={styles.containerCaption}>
+                    <Text style={styles.caption}>Password</Text>
+                    {passwordError &&
+                        <Text style={styles.error}>{passwordError}</Text>}
+                </View>
+                
+                <TextInput
+                    style={styles.input}
+                    value={password}
+                    onChangeText={changePassword}
+                    autoComplete="new-password"
+                    autoCorrect={false}
+                    autoCapitalize="none"
+                    secureTextEntry
+                />
+            </View>
+
+            <View style={styles.containerInput}>
+                <View style={styles.containerCaption}>
+                    <Text style={styles.caption}>Confirm Password</Text>
+                    {passwordVerifyError &&
+                        <Text style={styles.error}>{passwordVerifyError}</Text>}
+                </View>
+                
+                <TextInput
+                    style={styles.input}
+                    value={passwordVerify}
+                    onChangeText={changePasswordVerify}
+                    autoComplete="new-password"
+                    autoCorrect={false}
+                    autoCapitalize="none"
+                    secureTextEntry
+                />
+            </View>
+
             <TouchableOpacity onPress={close}>
                 <Text>Close</Text>
             </TouchableOpacity>
--- src/screens/CreateAccountScreen/styles.ts
+++ src/screens/CreateAccountScreen/styles.ts
@@ -1,7 +1,7 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 26 Nov 2024, 1:17:01 PM
- *  Last update: 
+ *  Last update: 26 Nov 2024, 1:33:40 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { StyleSheet } from "react-native";
@@ -9,5 +9,39 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
     container: {
         minWidth: "85%",
+        gap: 15,
+        padding: 15,
+    },
+
+    containerInput: {
+        width: "100%",
+        gap: 8,
+    },
+
+    containerCaption: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingVertical: 2,
+    },
+
+    caption: {
+        fontSize: 17,
+    },
+
+    input: {
+        paddingVertical: 10,
+        paddingHorizontal: 7,
+
+        fontSize: 16,
+        
+        backgroundColor: "white",
+        borderWidth: 1,
+        borderColor: "#CCC",
+        borderRadius: 5,
+    },
+    
+    error: {
+        fontSize: 17,
+        color: "#D00",
     },
 });
