ae6ff7243d2665553569c72489fed7eae5edd89f
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-26 18:52:59 -0500

    Tweak styles to make list always the full screen

--- src/components/events/EventList/styles.ts
+++ src/components/events/EventList/styles.ts
@@ -1,14 +1,14 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 26 Nov 2024, 3:30:44 PM
- *  Last update: 26 Nov 2024, 3:55:50 PM
+ *  Last update: 26 Nov 2024, 6:51:37 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
     container: {
-        minWidth: "100%",
+        flex: 1,
     },
 
     list: {
