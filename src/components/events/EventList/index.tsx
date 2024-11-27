1136a14ef6aa0f13c449adc004d363fe4e2c1406
Kaleb Jubar <k_jubar@fanshaweonline.ca>
2024-11-26 21:06:59 -0500

    Add create button and empty add modal

--- src/components/events/EventList/index.tsx
+++ src/components/events/EventList/index.tsx
@@ -1,15 +1,20 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 26 Nov 2024, 3:30:44 PM
- *  Last update: 26 Nov 2024, 7:03:09 PM
+ *  Last update: 26 Nov 2024, 9:05:54 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
-import { View, FlatList, Text } from "react-native";
+import { useState } from "react";
+
+import { View, FlatList, Text, TouchableHighlight, TouchableOpacity } from "react-native";
 
 import { Event } from "../../../data/firebase/config";
 import { useEventList, useFavorites } from "../../../data/state/hooks";
 
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
 import { EventListItem } from "./EventListItem";
+import { GenericModal } from "../../common/GenericModal";
 
 import styles from "./styles";
 
@@ -21,6 +26,7 @@ interface Props {
 }
 
 export function EventList({ onPressEventItem, favoritesOnly = false }: Props) {
+    const [addShown, setAddShown] = useState<boolean>(false);
     const events = useEventList();
     const favorites = useFavorites();
 
@@ -33,11 +39,20 @@ export function EventList({ onPressEventItem, favoritesOnly = false }: Props) {
                 )}
                 keyExtractor={(event) => event.id}
                 showsVerticalScrollIndicator={false}
-                contentContainerStyle={styles.list}
+                contentContainerStyle={[styles.list, !favoritesOnly ? styles.listWithAdd : undefined]}
                 ListEmptyComponent={
                     <Text style={styles.empty}>No events found! {favoritesOnly ? "Try adding some favorites." : "Try creating your own."}</Text>
                 }
             />
+
+            {!favoritesOnly &&
+                <TouchableHighlight style={styles.addBtn} onPress={() => setAddShown(true)} underlayColor="#0D0">
+                    <MaterialIcons name="add" size={36} color="white" />
+                </TouchableHighlight>}
+
+            <GenericModal visible={addShown}>
+                <TouchableOpacity onPress={() => setAddShown(false)}><Text>Close</Text></TouchableOpacity>
+            </GenericModal>
         </View>
     );
 }
--- src/components/events/EventList/styles.ts
+++ src/components/events/EventList/styles.ts
@@ -1,20 +1,26 @@
 /*
  *  Author: Kaleb Jubar
  *  Created: 26 Nov 2024, 3:30:44 PM
- *  Last update: 26 Nov 2024, 6:51:37 PM
+ *  Last update: 26 Nov 2024, 9:05:34 PM
  *  Copyright (c) 2024 Kaleb Jubar
  */
 import { StyleSheet } from "react-native";
 
+import { lightDropShadowStyle } from "../../../util/constants";
+
 export default StyleSheet.create({
     container: {
+        position: "relative",
         flex: 1,
     },
 
     list: {
         padding: 20,
-        paddingBottom: 75,
         gap: 15,
+    },
+
+    listWithAdd: {
+        paddingBottom: 75,
     },
 
     header: {
@@ -24,5 +30,17 @@ export default StyleSheet.create({
 
     empty: {
         fontSize: 18,
+    },
+
+    addBtn: {
+        position: "absolute",
+        bottom: 6,
+        right: 20,
+        padding: 12,
+    
+        backgroundColor: "#0A0",
+        borderRadius: 360,
+
+        ...lightDropShadowStyle,
     },
 });
